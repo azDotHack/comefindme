38ea1eb370d95f52c2abeec94deddbe7f08587cd
Tyman Sin <sin.tyman@yahoo.com>
2017-01-14 16:56:30 -0700

    Added editprofile.php, wrote basic HTML structure for index_signed_in.php

--- index_signed_in.php
+++ index_signed_in.php
@@ -1,1 +1,19 @@
 <h2>Hooray for SpongeBOOOOOOOOB!!!</h2>
+<div>
+	<h3>New Job Matches</h3>
+</div>
+<div>
+	<h3>New Client Matches</h3>
+</div>
+<div>
+	<h3>My Profile</h3>
+	
+	<!-- display name, profile pic, current jobs, current listings etc. -->
+	<?php echo "Hello world and all who inhabit it";?>
+	
+	
+	<form action= <?php ?>>
+		<input type="submit" value="Edit My Profile" name=editprofile>
+		<input type="submit" value="Add a Job" name=addjob>
+	</form>
+</div>
