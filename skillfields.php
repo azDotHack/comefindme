86aa82b5cec804cf5e85885760b87aa151ff3b09
Tyman Sin <sin.tyman@yahoo.com>
2017-01-14 15:39:52 -0700

    Added skillfields.php

---
+++ skillfields.php
@@ -0,0 +1,21 @@
+<form action="index.php">
+	<select name="skill_fields" id = "skill_fields">
+		<option value="default">--Pick a field--</option>
+		<option value="agri">Agriculture, Food &amp; Natural Resources </option>
+		<option value="arch">Architecture &amp; Construction</option>
+		<option value="arts">Arts, Audio/Video Technology &amp; Communications</option>
+		<option value="busi">Business, Management &amp; Administration</option>
+		<option value="educ">Education &amp; Training</option>
+		<option value="fina">Finance</option>
+		<option value="gove">Government &amp; Public Administration</option>
+		<option value="heal">Health Science</option>
+		<option value="hosp">Hospitality &amp; Tourism</option>
+		<option value="huma">Human Services</option>
+		<option value="it">Information Technology</option>
+		<option value="law">Law, Public Safety, Corrections &amp; Security</option>
+		<option value="manu">Manufacturing</option>
+		<option value="mark">Marketing, Sales &amp; Service</option>
+		<option value="stem">Science, Technology, Engineering &amp; Mathematics</option>
+		<option value="tran">Transportation, Distribution &amp; Logistics</option>
+	</select>
+</form>
