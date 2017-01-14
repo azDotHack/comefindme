aa242a0d4cb67b65dcef7bd1f59fbdb59b034470
bull610 <blladi610@yahoo.com>
2017-01-14 09:54:51 -0700

    did the boiler code for register.php

---
+++ register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Come Find Me!</title>
+	<meta charset="UTF-8">
+	<meta name="author" content="arizonaDotHack">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<link rel="stylesheet" href="style.css">
+	<script></script>
+</head>
+<body>
+
+</body>
+</html>
