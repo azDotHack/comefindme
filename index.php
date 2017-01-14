21585171b03566d6898db71eebf587a36099cd3b
bull610 <blladi610@yahoo.com>
2017-01-14 11:15:40 -0700

    created a sign in and sign up field validated by JS

--- index.php
+++ index.php
@@ -1,24 +1,80 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Come Find Me!</title>
-	<meta charset="UTF-8">
-	<meta name="author" content="arizonaDotHack">
-	<meta name="keywords" content="">
-	<meta name="description" content="">
-	<link rel="stylesheet" href="style.css">
-	<script></script>
+<title>Come Find Me!</title>
+<meta charset="UTF-8">
+<meta name="author" content="arizonaDotHack">
+<meta name="keywords"
+	content="come, find, me, search, jobs, individuals">
+<meta name="description"
+	content="This is a website for individuals to find others for basic jobs such as babysitting, lawnmowing, and more!">
+<link rel="stylesheet" href="style.css">
+<script>
+		//clear out an element
+		function deleteChildren(id) {
+			var elem = document.getElementById(id);
+			while (elem.firstChild)
+				elem.removeChild(elem.firstChild);
+		}
+
+		//checks if a certain input field is empty
+		function isEmptyField(id) {
+			var input = document.getElementById(id);
+			if (input.value == "")
+				return true;
+			return false;
+		}
+
+		//check to make sure submitted image is valid
+		function verifyPicture() {
+			//save the value of the image feild as well as the file extension in two variables
+			var file = document.getElementById("signup_pic").value,
+				extension = file.substring(file.length - 3);
+
+			//check to see if the file extension is a valid one
+			if (extension == "peg" || extension == "jpg" || extension == "png" || extension == "gif")
+				return true;
+
+			//this will run if the extension is invalid
+			alert("Error! Make sure your image file is a valid one!");
+			return false;
+		}
+
+		//function that takes two input fields as input and returns true if they are both filled out but false otherwise
+		function verifyTextInput(id1, id2) {
+			var isValid = true;
+			//check to see if the two specified fields are empty
+			if (isEmptyField(id1) || isEmptyField(id2))
+				isValid = false;
+
+			//send an error alert if the form is invalid
+			if (!isValid)
+				alert("Error! Make sure all fields in the sign up section are filled out!");
+
+			return isValid;
+		}
+		
+		window.onload = function() {
+			document.getElementById("signin_form").onsubmit = function() {
+				return verifyTextInput("signin_username", "signin_pwd");
+			};
+
+			document.getElementById("signup_form").onsubmit = function() {
+				return verifyTextInput("signup_firstname", "signup_lastname") && verifyPicture();
+			};
+		};
+	</script>
 </head>
 <body>
-<?php
-	//if some is logged in (there is a current session going on)
-	if (isset($_SESSION['username'])) {
-		//request the index_signed_in page
-		require_once("index_signed_in.php");
+	<?php
+	// if some is logged in (there is a current session going on)
+	if (isset ( $_SESSION ['username'] )) {
+		// request the index_signed_in page
+		require_once ("index_signed_in.php");
 	} else {
-		//request the index_signed_out page
-		require_once("index_signed_out.php");
+		// request the index_signed_out page
+		require_once ("index_signed_out.php");
 	}
-?>
+	?>
 </body>
 </html>
--- index_signed_out.php
+++ index_signed_out.php
@@ -1,5 +1,21 @@
 <!-- Sign in field -->
 <h3>Sign In</h3>
-
+<form id="signin_form" action="#" method="post">
+	<!-- Username and password fields go here -->
+	<div>
+		<label>Username: </label> <input type="text" name="signin_username" id="signin_username"> <br>
+		<label>Password: </label> <input type="password" name="signin_pwd" id="signin_pwd"> <br>
+		<input type="submit" value="Sign In" id="signin_submit">
+	</div>
+</form>
 <!-- Sign up field -->
-<h3>Sign In</h3>
+<h3>Register</h3>
+<form id="signup_form" action="register.php" method="post">
+	<!-- Initial sign up components (first name, last name, and profile picture) are submitted here -->
+	<div>
+		<label>First Name: </label> <input type="text" name="signup_firstname" id="signup_firstname"> <br>
+		<label>Last Name: </label> <input type="text" name="signup_lastname" id="signup_lastname"> <br>
+		<label>Profile picture: </label> <input type="file" name="signup_pic" id="signup_pic"> <br>
+		<input type="submit" value="Continue" id="signup_submit">
+	</div>
+</form>
