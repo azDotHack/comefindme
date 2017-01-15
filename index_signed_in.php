2889d6f52682a87c7fa5eb9cc6aa4398b9124543
Tyman Sin <sin.tyman@yahoo.com>
2017-01-14 23:25:51 -0700

    changed directing infrastructure, added form in skill.php

--- index_signed_in.php
+++ index_signed_in.php
@@ -4,22 +4,20 @@
 	
 	<!-- get top 10 job.php matches for this user -->
 	<?php ?>
-	
-	<a href="joblist.php">Click to see more jobs</a>
+	<!-- ADD CODE TO LOAD MORE JOBS -->
 </div>
 <div>
 	<h3>New Employee Matches</h3>
 	
 	<!-- get top 10 skill.php matches for this user -->
 	<?php ?>
-	
-	<a href="employeelist.php">Click to see more employees</a>
+	<!-- ADD CODE TO LOAD MORE SKILLS -->
 </div>
 <div>
 	<h3>My Profile</h3>
 	
-	<!-- display name, profile pic, current jobs, current listings etc. -->
-	<?php echo "Hello world and all who inhabit it";?>
+	<!-- display name, profile pic, job.php and skill.php with contact requests -->
+	<?php ?>
 	
 	<!-- get direct to either edit profile or add a job -->
 	<form action="profile.php">
