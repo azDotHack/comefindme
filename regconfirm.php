3cc0794718ed45f55d2c8948d579914c417eb96b
bull610 <blladi610@yahoo.com>
2017-01-14 21:48:23 -0700

    Stored new user variables and generated random auth code for the regsubmit.php file

--- regconfirm.php
+++ regconfirm.php
@@ -9,11 +9,31 @@
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-		<h1><strong>CONGLATURATION! YOU'RE WINNER!</strong></h1>
-		<form action="index.php">
-			Enter the code to join Come Find Me!<br>
-			<input type="text" name="confirm_code">
-			<input type="submit" value="submit">
-		</form>
+		<?php
+			//function to generate random auth code of 8 alphanumeric characters
+			function generateAuth() {
+				$chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890;";
+				$i = 0;
+				$code = "";
+				while ($i < 8) {
+					$index = rand(0, strlen($chars) - 1);
+					$code += substr($chars, $index, $index + 1);
+					++$i;
+				}
+				
+				return $code;
+			}
+		
+			//get all of the variables needed to create a profile
+			$first_name = $_POST['signup_firstname'];
+			$last_name = $_POST['signup_firstname'];
+			$email = $_POST['signup_firstname'];
+			$password = $_POST['signup_firstname'];
+			$phone_number = $_POST['signup_firstname'];
+			$state = $_POST['signup_firstname'];
+			$zip_code = $_POST['signup_firstname'];
+			$auth_code = generateAuth();
+			$activated = 0;
+		?>
 	</body>
 </html>
